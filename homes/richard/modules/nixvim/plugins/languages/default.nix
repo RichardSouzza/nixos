1771a9b1e37c55abb4c8dde4267fc747437fa795
Richard Souza <souzza.richard25@gmail.com>
2025-06-02 20:23:43 -0300

    home: Add keymaps for NeoVim CMP

--- homes/richard/modules/nixvim/plugins/languages/default.nix
+++ homes/richard/modules/nixvim/plugins/languages/default.nix
@@ -4,13 +4,66 @@
     ./typescript.nix
   ];
 
-  programs.nixvim.plugins = {
-    cmp.enable = true;
+  programs.nixvim = {
+    plugins = {
+      cmp = {
+        enable = true;
+        
+        settings.sources = [
+          { name = "nvim_lsp"; }
+          { name = "path";     }
+          { name = "buffer";   }
+        ];
+      };
 
-    lsp = {
-      enable = true;
-      autoLoad = true;
-      inlayHints = true;
+      lsp = {
+        enable = true;
+        autoLoad = true;
+        inlayHints = true;
+      };
     };
+
+    keymaps = [
+      {
+        mode = "i";
+        key = "<A-Left>";
+        lua = true;
+        action = ''
+          function()
+            local cmp = require("cmp")
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              cmp.complete()
+            end
+          end
+        '';
+        options = {
+          noremap = true;
+          silent = true;
+          desc = "Previous CMP suggestion";
+        };
+      }
+      {
+        mode = "i";
+        key = "<A-Right>";
+        lua = true;
+        action = ''
+          function()
+            local cmp = require("cmp")
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              cmp.complete()
+            end
+          end
+        '';
+        options = {
+          noremap = true;
+          silent = true;
+          desc = "Next CMP suggestion";
+        };
+      }
+    ];
   };
 }
