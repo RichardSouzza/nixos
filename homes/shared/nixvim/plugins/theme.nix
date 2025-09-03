be7b8657769d15bc0b9de57486967db902383866
Richard Souza <souzza.richard25@gmail.com>
2025-09-03 20:45:27 -0300

    nvim: Add more plugins
    Bracketpair and Gitsigns

--- homes/shared/nixvim/plugins/theme.nix
+++ homes/shared/nixvim/plugins/theme.nix
@@ -1,9 +1,31 @@
+{ pkgs, ... }:
+
 {
-  programs.nixvim.colorschemes = {
-    nightfox = {
-      enable = true;
-      flavor = "carbonfox";
-      settings.options.transparent = true;
+  programs.nixvim = {
+    colorschemes = {
+      nightfox = {
+        enable = true;
+        flavor = "carbonfox";
+        settings = {
+          options.transparent = true;
+        };
+      };
     };
+
+    plugins = {
+      gitsigns.enable = true;
+    };
+
+    extraPlugins = with pkgs; [
+      (vimUtils.buildVimPlugin {
+        name = "bracketpair";
+        src = fetchFromGitHub {
+          owner = "fedepujol";
+          repo = "bracketpair.nvim";
+          rev = "805b233f4fc1a36706e6fbc7259a244777677a53";
+          hash = "sha256-iOAqLlj7RMMj93gxxxEIyL1ReOreBpSnXfpk1jJA1CE=";
+        };
+      })
+    ];
   };
 }
