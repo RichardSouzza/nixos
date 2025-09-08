35430561831ae2f439c6fdf023575c10502b038c
Richard Souza <souzza.richard25@gmail.com>
2025-09-08 19:50:17 -0300

    home: Fix ZSH keybinds

--- homes/shared/zsh/default.nix
+++ homes/shared/zsh/default.nix
@@ -9,28 +9,28 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      act  = "sudo /etc/nixos/hosts/activation.sh";
-      c    = "tput clear";
-      cc   = "cd /etc/nixos";
-      db   = "dbeaver";
-      dok  = "lazydocker";
-      e    = "superfile";
-      g    = "lazygit";
-      gg   = "gitui";
-      hms  = "home-manager switch --flake /etc/nixos/#${username}@${hostname} --impure";
-      hr   = "hyprctl reload";
-      ls   = "lsd --group-directories-first";
-      manc = "man configuration.nix";
-      manh = "man home-configuration.nix";
-      mann = "man nixvim";
-      nsh  = "nix-shell --command 'zsh'";
-      s    = "sudo -i";
-      rm   = "gtrash put";
-      rs   = "gtrash restore";
+      act   = "sudo /etc/nixos/hosts/activation.sh";
+      c     = "tput clear";
+      cc    = "cd /etc/nixos";
+      db    = "dbeaver";
+      dok   = "lazydocker";
+      e     = "superfile";
+      g     = "lazygit";
+      gg    = "gitui";
+      hms   = "home-manager switch --flake /etc/nixos/#${username}@${hostname} --impure";
+      hr    = "hyprctl reload";
+      ls    = "lsd --group-directories-first";
+      manc  = "man configuration.nix";
+      manh  = "man home-configuration.nix";
+      mann  = "man nixvim";
+      nsh   = "nix-shell --command 'zsh'";
+      s     = "sudo -i";
+      rm    = "gtrash put";
+      rs    = "gtrash restore";
       dtree = "lsd --tree";
       tree  = "tree --gitignore --dirsfirst";
-      v    = "nvim .";
-      y    = "yy";
+      v     = "nvim .";
+      y     = "yy";
     };
 
     initContent = ''
@@ -45,13 +45,14 @@
           fi
           rm -f -- "$tmp"
       }
-    '';
-  };
 
-  home.file = {
-    ".inputrc".text = ''
-      "\e[1;5D": backward-word
-      "\e[1;5C": forward-word
+      # Configure keybinds
+      bindkey -e
+
+      bindkey "^[OH"    beginning-of-line
+      bindkey "^[OF"    end-of-line
+      bindkey "^[[1;5D" backward-word
+      bindkey "^[[1;5C" forward-word
     '';
   };
 }
