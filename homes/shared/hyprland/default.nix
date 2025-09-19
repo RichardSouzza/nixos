a20ac34942ae28cb1337e06be3987887e3ce2ea1
Richard Souza <souzza.richard25@gmail.com>
2025-09-19 19:57:39 -0300

    home: Change Hyprpaper declaration to script

--- homes/shared/hyprland/default.nix
+++ homes/shared/hyprland/default.nix
@@ -1,19 +1,21 @@
+{ lib, username, ... }:
+
+let
+  hyprpaperConfig = "/home/${username}/.config/hypr/hyprpaper.conf";
+
+in
 {
   home.file = {
     ".config/hypr/hyprland.conf".source = ./hyprland.conf;
   };
-
-  services.hyprpaper = {
-    enable = true;
-
-    settings = {
-      preload = [
-        "~/imgs/wallpapers/mountains.jpg"
-      ];
-
-      wallpaper = [
-        "eDP-1, ~/imgs/wallpapers/mountains.jpg"
-      ];
-    };
-  };
 }
+// (if !builtins.pathExists hyprpaperConfig then
+{
+  home.activation.mkHyprpaperConfig =
+    lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+      cat <<-EOF > ~/.config/hypr/hyprpaper.conf
+      preload=~/imgs/wallpapers/
+      wallpaper=eDP-1, ~/imgs/wallpapers/
+      EOF
+    '';
+} else {})
