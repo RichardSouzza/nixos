57cc393571bf9154baf6cd3be3cb1c12d4739cdb
Richard Souza <souzza.richard25@gmail.com>
2025-09-19 19:58:54 -0300

    nvim: Configure Visual Multi

--- homes/shared/nixvim/plugins/editor.nix
+++ homes/shared/nixvim/plugins/editor.nix
@@ -4,6 +4,26 @@
   programs.nixvim = {
     plugins = {
       trim.enable = true;
+
+      visual-multi = {
+        enable = true;
+        settings = {
+          default_mappings = 0;
+          maps = {
+            "Add Cursor Up"        = "<C-Up>";
+            "Add Cursor Down"      = "<C-Down>";
+            "Align"                = "<A-a>";
+            "Case Conversion Menu" = "<A-c>";
+            "Find Prev"            = "<C-[>";
+            "Find Under"           = "<C-]>";
+            "Move Left"            = "<A-Left>";
+            "Move Right"           = "<A-Right>";
+          };
+          mouse_mappings = 1;
+          show_warnings = 1;
+          silent_exit = 0;
+        };
+      };
     };
 
     extraPlugins = with pkgs; [
