6aaac9bf7b72fe732ae3913f8706c2cf02874d08
Richard Souza <souzza.richard25@gmail.com>
2025-09-24 14:55:05 +0000

    nvim: Test downgrade to v0.10.4

--- homes/shared/nixvim/default.nix
+++ homes/shared/nixvim/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./keymaps
@@ -6,6 +8,21 @@
 
   programs.nixvim = {
     enable = true;
+
+    # package = pkgs.neovim-unwrapped.overrideAttrs (old: {
+    #   version = "v0.10.4";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "neovim";
+    #     repo = "neovim";
+    #     rev = "e96f75a4e60c9082e89c7f61e2ce0647e4ebdf43";
+    #     hash = "sha256-TAuoa5GD50XB4OCHkSwP1oXfedzVrCBRutNxBp/zGLY=";
+    #   };
+    #   buildInputs = with pkgs; old.buildInputs ++ [
+    #     libvterm-neovim
+    #     msgpack-c
+    #   ];
+    # });
+
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
