d95711005fbcf86347590b1464bd34946c0358e6
Richard Souza <souzza.richard25@gmail.com>
2025-10-07 11:30:29 +0000

    nvim: Configure Spider

--- homes/shared/nixvim/plugins/editor.nix
+++ homes/shared/nixvim/plugins/editor.nix
@@ -11,6 +11,17 @@ in
   programs.nixvim = {
     plugins = {
       guess-indent.enable = true;
+
+      spider = {
+        enable = true;
+        settings = {
+          customPatterns = {
+            patterns = [ ";" ];
+            overrideDefault = false;
+          };
+        };
+      };
+
       trim.enable = true;
 
       visual-multi = {
@@ -44,6 +55,41 @@ in
           hash = "sha256-k3Q7mk04dd2yql4NK5Lrm/AEy3lWp0xV2qvxqVly3lg=";
         };
       })
+    ];
+
+    keymaps = [
+      {
+        action = "<CMD>lua require('spider').motion('b')<CR>";
+        key = "<C-left>";
+        mode = "n";
+        options = {
+          desc = "Go to the beginning of the previous word";
+        };
+      }
+      {
+        action = "<CMD>lua require('spider').motion('w')<CR>";
+        key = "<C-right>";
+        mode = "n";
+        options = {
+          desc = "Go to the beginning of the next word.";
+        };
+      }
+      {
+        action = "<ESC>l<CMD>lua require('spider').motion('b')<CR>i";
+        key = "<C-left>";
+        mode = "i";
+        options = {
+          desc = "Go to the beginning of the previous word";
+        };
+      }
+      {
+        action = "<ESC>l<CMD>lua require('spider').motion('w')<CR>i";
+        key = "<C-right>";
+        mode = "i";
+        options = {
+          desc = "Go to the beginning of the next word.";
+        };
+      }
     ];
   };
 }
